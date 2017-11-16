b929860dfd5e17f3b01076c4caf91616da6abb13
jamespeterson <jamespeterson0918@yahoo.com>
2017-11-16 15:34:36 -0500

    fleshed out final info for BIO tab

--- client/src/components/Bio.js
+++ client/src/components/Bio.js
@@ -29,9 +29,16 @@ class Bio extends Component {
                   <p>My name is James Peterson and I am a full-stack Web Application Developer currently living in Atlanta, Georgia. I have a Web Development certification from GA Assembly in Atlanta. In my spare time you can find me editing videos using Adobe Premeire or editing music using Adobe Audition.
                    My only passion in life is to create and I think that without the ability and freedom to create any piece of art, one would go insane. 
                    "If you want to be creative don’t worry about style or what is trending. Focus on what you want to achieve and do it. Don’t worry about the veneer and packaging till after." - Vincent Van Gogh</p>
+                 
                   <h2>Languages</h2>
                   <p>I started studying web development in August of 2017 and completed a 3 month immersive course in order to receive my certification, and my primary choice for development is React on Rails. I have experience in both front and back-end development, and also have efficient experience with 
-                      Javascript, jQuery, Node ,SQL and Postgresql, Ajax ,HTML5, and CSS3.</p> 
+                      Javascript, jQuery, Node ,SQL and Postgresql, Ajax ,HTML5, and CSS3.</p>
+
+                <h3>Experience</h3>
+                <p></p>
+
+                <h4>Technologies Used for Portfolio</h4>
+                 
     <ImgWrapper src="https://i.imgur.com/KNhmwJD.jpg"/>  
        </FlexContainer> 
             </div>
