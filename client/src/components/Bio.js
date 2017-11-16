c9077ba56d0fcd2b2c7c52e2c5880a9a748da987
jamespeterson <jamespeterson0918@yahoo.com>
2017-11-16 15:15:54 -0500

    adding more bio text in tbio tab

--- client/src/components/Bio.js
+++ client/src/components/Bio.js
@@ -9,7 +9,7 @@ const FlexContainer = styled.div`
 display: flex;
 justify-content: space-between; 
 align: center;
-background: black;
+
 padding: 15px;
 `
 const ImgWrapper = styled.img`
@@ -24,10 +24,10 @@ class Bio extends Component {
             <div>
              <FlexContainer>
     <p class="bio">My name is James Peterson and I am a full-stack Web Application Develope,r currently living in Atlanta, Georgia. I have a Web Development certification from GA Assembly in Atlanta. In my spare time you can find me editing videos using Adobe Premeire or editing music using Adobe Audition.
-                   My only passion in life is to create and I think that without the ability and freedom to create any piece of art, one would go insane.</p>
-                   <p>"If you want to be creative don’t worry about style or what is trending. Focus on what you want to achieve and do it. Don’t worry about the veneer and packaging till after." - Vincent Van Gogh</p>
+                   My only passion in life is to create and I think that without the ability and freedom to create any piece of art, one would go insane. 
+                   "If you want to be creative don’t worry about style or what is trending. Focus on what you want to achieve and do it. Don’t worry about the veneer and packaging till after." - Vincent Van Gogh</p>
    
-    <ImgWrapper src="https://i.imgur.com/NyG8ZJV.jpg"/>  
+    <ImgWrapper src="https://i.imgur.com/KNhmwJD.jpg"/>  
        </FlexContainer> 
             </div>
         );
