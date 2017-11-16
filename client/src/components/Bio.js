179a359b253c400a136b4f18196e9166e2cb21b2
jamespeterson <jamespeterson0918@yahoo.com>
2017-11-16 15:21:10 -0500

    got picture positioned good for time being in bio

--- client/src/components/Bio.js
+++ client/src/components/Bio.js
@@ -13,9 +13,11 @@ align: center;
 padding: 15px;
 `
 const ImgWrapper = styled.img`
-width: 45%;
+width: 35%;
 margin: 15px;
 height: 60%;
+padding: 485px;
+margin-right: -25%
 `
 
 class Bio extends Component {
@@ -24,7 +26,7 @@ class Bio extends Component {
             <div>
             <Header />
              <FlexContainer>
-    <p class="bio">My name is James Peterson and I am a full-stack Web Application Develope,r currently living in Atlanta, Georgia. I have a Web Development certification from GA Assembly in Atlanta. In my spare time you can find me editing videos using Adobe Premeire or editing music using Adobe Audition.
+    <p class="bio">My name is James Peterson and I am a full-stack Web Application Developer currently living in Atlanta, Georgia. I have a Web Development certification from GA Assembly in Atlanta. In my spare time you can find me editing videos using Adobe Premeire or editing music using Adobe Audition.
                    My only passion in life is to create and I think that without the ability and freedom to create any piece of art, one would go insane. 
                    "If you want to be creative don’t worry about style or what is trending. Focus on what you want to achieve and do it. Don’t worry about the veneer and packaging till after." - Vincent Van Gogh</p>
    
