10ca20de177d7bef99c3dc97fa6ef6d887f392f6
jamespeterson <jamespeterson0918@yahoo.com>
2017-11-17 10:20:27 -0500

    somewhat satisfied with contact page

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -5,7 +5,11 @@ import styled from 'styled-components'
 import Header from './Header'
 
 const FlexContainer = styled.div`
-
+display: block-flex;
+margin-left: 40%;
+padding: 20px;
+float: center;
+aign-items: center;
 `
 const ImgWrapper = styled.img`
 display: flex;
@@ -25,9 +29,9 @@ class Contacts extends Component {
              <h3>Contact Me:</h3>
              <p>jamespeterson0918@yahoo.com</p>
              <a href="https://github.com/timothygein">Github</a>
-                 
+              </FlexContainer>   
     <ImgWrapper src="https://i.imgur.com/I5wUFAx.png"/>  
-       </FlexContainer> 
+        
             </Wrapper>
         );
     }
