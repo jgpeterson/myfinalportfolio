86ff861f37a3a2741766494d90ea1fd6051aeb3c
jamespeterson <jamespeterson0918@yahoo.com>
2017-11-17 15:07:39 -0500

    ready to go with resume download

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -5,11 +5,12 @@ import styled from 'styled-components'
 import Header from './Header'
 
 const FlexContainer = styled.div`
-display: block-flex;
-margin-left: 40%;
-padding: 20px;
-float: center;
-aign-items: center;
+display: flex;
+justify-content: space-between;
+padding: 150px;
+align-items: center;
+flex-direction: column;
+text-align: center;
 `
 const Wrapper = styled.div`
 
@@ -21,7 +22,7 @@ class Contacts extends Component {
             <Header />
              <FlexContainer>
              <h3>Click below to Download my Resume</h3>
-             <a href="path_to_file" download="proposed_file_name">Download</a>
+             <p><a href="path_to_file" download="proposed_file_name">Download</a></p>
             
               </FlexContainer>   
     
