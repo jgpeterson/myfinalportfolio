bd24799ff5eba8d533442288d0e0cb2675eefe7f
jamespeterson <jamespeterson0918@yahoo.com>
2017-11-20 12:14:51 -0500

    reworked the header styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
+const Bar = styled.div`
+align-items: center;
+flex-direction: row;
+justify-content: space-between;
+padding: 0px 40px;
+border-bottom: 1px solid #DCDCDC :
+margin-bottom: 60px;
+height: 77px; 
+` 
 
 
 class Header extends Component {
     render() {
         return (
         
-            <div>
+            <Bar>
             <div className="container">
             <div className="item">
               <Link to="/bio">About Me</Link>
@@ -20,8 +29,7 @@ class Header extends Component {
             <Link to="/contacts">Resume</Link>
             </div>
             </div>
-            </div>
-            
+            </Bar>
         );
     }
 }
