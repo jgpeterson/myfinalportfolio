06da87e54648021b9b2f3e5257e4004b11b4f3d4
jamespeterson <jamespeterson0918@yahoo.com>
2018-02-25 15:39:01 -0500

    changd home photo again

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -19,7 +19,7 @@ class HomePage extends Component {
             
         <FlexContainer>
             <div class="photo">
-       <img src="https://i.imgur.com/gglNXL4.jpg"/>
+       <img src="https://i.imgur.com/tz7l7bP.jpg"/>
        <div class="james">
        <p>James Peterson</p>
        <p>Full-Stack Web Developer</p>
